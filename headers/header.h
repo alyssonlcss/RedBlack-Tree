539230769cda880568919dfe81b4062d1efc375e
alyssonlcss <alyssonlcss@gmail.com>
2022-02-08 17:20:40 -0300

    all ready

--- headers/header.h
+++ headers/header.h
@@ -5,6 +5,7 @@
 #define _10K 10000
 #define _100K 100000
 
+
 struct node {
     int key;
     char color;
@@ -14,18 +15,22 @@ struct node {
 };
 typedef struct node Node;
 
-Node *createNode(int key);
-void moveDad(Node *u, Node *v, Node **ptroot);
-void leftRotate(Node **pt, int *h);
-void rightRotate(Node **pt, int *h);
-void insertRB(int inKey, Node **pt, int *h);
-void removeRB(int x, Node **pt, int *h);
-int height(Node *pt);
-int checkRB(Node *pt);
-void countNodes(Node *pt, int *sum);
-void outputAVL(Node *pt);
-void freeAVL(Node **pt);
+
+Node *createNode(int key, Node *outside);
+void moveDad(Node *u, Node *v, Node **root, Node *outside);
+Node *external();
+void leftRotate(Node *new, Node **root, Node *outside);
+void rightRotate(Node *new, Node **root, Node *outside);
+void rotaRN(Node *new, Node **root, Node *outside);
+Node* searchNodede(Node* aux, int key, Node *outside);
+Node* successor(Node *z, Node* outside);
+void routeRemoveRB(Node *x, Node **root, Node *outside);
+void removeRB(Node* trash, Node **root, Node *outside);
+void insertRB(int new_key, Node **root, Node *outside);
+void countNodes(Node *root, int *sum, Node *outside);
+void tests(Node **root, int *vec, int *sum, Node *outside);
+void freeRB(Node **root, Node *outside);
 void paInVec(int *vec, int seed);
-void tests(Node **pt, int *vec, int *sum, int *h);
+
 
 #endif
